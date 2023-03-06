1086dc67ff77c8a3d43630ec5292c35d11999c14
Daniil Pisarev <akaipureya@ya.ru>
2023-03-06 18:12:05 +0300

    Add log_text function

--- stable/logger.py
+++ stable/logger.py
@@ -1,10 +1,3 @@
-# def log(text, role, user_id ,filename="logs.txt"):
-#     with open(filename, 'a') as file:
-#         if role == "Bot":
-#             file.write("<" + str(time()) + " " + role + " " + str(user_id) + "> "+text + '\n\n')
-#         else:
-#             file.write("<" + str(time()) + " " + role + " " + str(user_id) + "> "+text + '\n')
-
 import time
 from chat_user import ChatUser
 LOGS_PATH = "logs/"
@@ -23,10 +16,3 @@ def log_text(text, user: ChatUser , role= "Bot", filename= LOGS_PATH + "logs.txt
             timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
             log_line = "[TEXT LOG] <" + timestamp + "> #Bot  " + "(User_name:" + user.user_name + "; User_id:" + str(user.user_id) + ") >> " + text.replace("\n", " ") + "\n\n"
             file.write(log_line)
-
-
-
-# chat_user = ChatUser("nasme",123321)
-
-# log_text("Ask",chat_user,"User")
-# log_text("hehe fdsfds fdsfds\n fdsfds\nfdsfdfdes\nfgdsfds",chat_user,"Bot")

bb8ebe4d07cbe58554fb05335de1a42a9aaa2963
Daniil Pisarev <akaipureya@ya.ru>
2023-03-07 00:01:16 +0300

    add migrations

---
+++ stable/migration.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+# Создаем подключение к базе данных
+conn = sqlite3.connect('payments.db')
+
+# Создаем курсор для выполнения операций с базой данных
+c = conn.cursor()
+
+# Создаем таблицу с указанными колонками
+c.execute('''CREATE TABLE IF NOT EXISTS payments
+             (id INTEGER PRIMARY KEY,
+              uuid TEXT,
+              user_id INTEGER,
+              user_name TEXT,
+              status TEXT,
+              created_at INTEGER,
+              payment_at INTEGER)''')
+
+# Сохраняем изменения в базе данных
+conn.commit()
+
+# Закрываем соединение с базой данных
+conn.close()

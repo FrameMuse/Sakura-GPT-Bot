126fb0c6e02611fd1c319eaa350cb2a8bb55419d
FrameMuse <minicablestone@gmail.com>
2023-03-09 04:57:28 +0300

    add `Serializable`

---
+++ stable/serializable.py
@@ -0,0 +1,26 @@
+import json
+
+class Serializable:
+    def __init__(self, fields_map: dict):
+        self.__fields_map = fields_map
+        self._fields = fields_map.keys()
+    
+    def _toJSON(self):
+        def filter_fields(cls) -> dict:          
+            if self != cls:
+                return cls.__dict__
+            
+            fields = {}
+            for key in self._fields:
+                fields[key] = str(cls.__dict__.get(key, ""))
+            return fields
+        
+        return json.dumps(self, default=filter_fields, indent=4)
+
+    def _assign(self, fields: dict):
+        for key, value in fields.items():
+            self.__dict__[key] = self.__fields_map[key](value)
+
+    def _assign_fromJSON(self, string: str):
+        json_content = json.loads(string)
+        return self._assign(json_content)

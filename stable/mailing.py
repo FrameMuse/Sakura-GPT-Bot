ba4ee61461e6e59716d928d8e553d5e07d63935f
FrameMuse <minicablestone@gmail.com>
2023-03-10 22:04:43 +0300

    1

--- stable/mailing.py
+++ stable/mailing.py
@@ -14,16 +14,53 @@ token = os.environ.get("TELEGRAM_KEY")
 
 bot = telebot.TeleBot(str(token))
 
-message_text = "❗️Если у вас закончатся токены, то вы можете пополнить баланс, нажав на кнопку ниже."
-keyboard = types.InlineKeyboardMarkup()
-donate_button = types.InlineKeyboardButton(text='🍩 Пополнить баланс', callback_data='donate')
-keyboard.add(donate_button)
+class ButtonTypes:
+    LINK = "link"
+    CALLBACK = "callback"
 
+class Mail:
+    
+    def __init__(self,text:str,button:bool, type, button_text="", button_callback_data="", button_link=""):
+        self.text = text
+        if not button:
+            return
+        
+        self.contain_button = button
+
+        self.keyboard = types.InlineKeyboardMarkup()
+
+        if type == "callback":
+            self.keyboard.add(types.InlineKeyboardButton(text=button_text, callback_data=button_callback_data))
+        if type == "link":
+            self.keyboard.add(types.InlineKeyboardButton(text=button_text, url=button_link))
+        
+    def send(self,bot:telebot.TeleBot, uid:int):
+        if not self.contain_button:
+            bot.send_message(uid, self.text,disable_notification=True)
+        else:
+            bot.send_message(uid, self.text, reply_markup=self.keyboard,disable_notification=True)
+        
+
+# message_text = "❗️Если у вас закончатся токены, то вы можете пополнить баланс, нажав на кнопку ниже❗️"
+# keyboard = types.InlineKeyboardMarkup()
+# donate_button = types.InlineKeyboardButton(text='🍩 Пополнить баланс', callback_data='donate')
+# keyboard.add(donate_button)
+
+mail = Mail(text="🥰Подпишитесь пожалуйста на наш телеграмм канал с новостями связанными с ботом, что бы ничего не упустить!🌸",
+            button=True,
+            type=ButtonTypes.LINK,
+            button_text="🌸Нажмите что-бы перейти🌸",
+            button_link="https://t.me/SakuraDevBlog")
+
+block_counter = 0
+sent_counter = 0
 for user_id in user_ids:
     try:
-        bot.send_message(user_id,message_text,reply_markup=keyboard)
+        mail.send(bot,user_id)
         print("Sent message to " + str(user_id))
+        sent_counter+=1
     except:
+        block_counter+=1
         print("Error occured while sending message to " + str(user_id))
 
-
+print(f"Mailing finished\nsent:{sent_counter}\nblocked:{block_counter}")

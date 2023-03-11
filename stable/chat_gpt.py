c9fbebc086e68976406ec727b5a75a7a3bb195b9
FrameMuse <minicablestone@gmail.com>
2023-03-11 09:51:36 +0300

    limit for chatGPT

--- stable/chat_gpt.py
+++ stable/chat_gpt.py
@@ -2,8 +2,6 @@ from restricted_words import RestrictedWords
 from personalities import Personality, Apology
 from db.repositories.openai_usage import OpenAIUsageRepository
 
-import re
-
 from dotenv import load_dotenv
 import os
 
@@ -12,6 +10,8 @@ import openai
 load_dotenv()
 
 openai.api_key = os.environ.get("OPEN_AI_KEY")
+
+RESPONSE_TOKENS_LIMIT = 2400
 
 def chatGPT(prompt: str, personality: Personality, previous_messages = []) -> str:
     # Restrict using some words.
@@ -34,11 +34,13 @@ def chatGPT(prompt: str, personality: Personality, previous_messages = []) -> st
     messages = start_messages + previous_messages + user_messages
 
     # Request to openai.
-    model = "gpt-3.5-turbo-0301"
+    model = "gpt-3.5-turbo"
     completion = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=personality.temperature
+        temperature=personality.temperature,
+        max_tokens=RESPONSE_TOKENS_LIMIT,
+        top_p=1
     )
     message = completion.choices[0]["message"]["content"] # type: ignore
 
@@ -49,27 +51,4 @@ def chatGPT(prompt: str, personality: Personality, previous_messages = []) -> st
 
     # Process the message.
     message_filtered = RestrictedWords.replace(message)
-
-    # Remove potenial [image description] in the message.
-    text_without_link = re.sub(r'\(.*?\)|https?://\S+', '', message_filtered)
-    return text_without_link
-
-def get_image(prompt: str):
-    prompt += ". Using modern technologies of photo illustration and have maximum details in the backround and foreground."
-    
-    if RestrictedWords.presence(prompt):
-        return
-    
-    response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size="256x256"
-    )
-
-     # Save information about usage.
-    repository = OpenAIUsageRepository()
-    repository.add(repository.Type.IMAGE, "dall-e")
-    repository.close()
-
-    image_url: str = response['data'][0]['url'] # type: ignore
-    return image_url
+    return message_filtered
